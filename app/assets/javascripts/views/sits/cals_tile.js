984120fdc6ee64e6d5f7db86616315c88a05ae2c
Vishak Visvanathan <vishak.visvanathan@gmail.com>
2015-08-20 16:40:49 -0700

    finish calories rescued graph FOREVER

--- app/assets/javascripts/views/sits/cals_tile.js
+++ app/assets/javascripts/views/sits/cals_tile.js
@@ -30,7 +30,8 @@ Sitbit.Views.CalsTile = Backbone.View.extend ({
   },
 
   vegaJson: function () {
-    var hourNow = (new Date(Date.now())).getHours() + 1;
+    var hourNow = (new Date(Date.now())).getHours();
+    var data = this.parseCalsDiffs();
 
     var graphSpecs = {
       "width": $("#cals-tile").width() - 60,
@@ -39,7 +40,11 @@ Sitbit.Views.CalsTile = Backbone.View.extend ({
       "data": [
         {
           "name": "table",
-          "values": this.parseCalsData()
+          "values": data[0]
+        },
+        {
+          "name": "integral",
+          "values": data[1]
         }
       ],
       "scales": [
@@ -50,16 +55,10 @@ Sitbit.Views.CalsTile = Backbone.View.extend ({
           "domain": {"data": "table", "field": "x"}
         },
         {
-          "name": "t",
-          "type": "linear",
-          "range": "height",
-          "domain": {"data": "table", "field": "t"},
-        },
-        {
           "name": "y",
           "type": "linear",
           "range": "height",
-          "domain": {"data": "table", "field": "y"},
+          "domain": this.graphDomain,
           "nice": true
         }
       ],
@@ -67,7 +66,7 @@ Sitbit.Views.CalsTile = Backbone.View.extend ({
         {
           "type": "x",
           "scale": "x",
-          "values": [0, 12, 23],
+          "values": [0, 6, 12, 18, 23],
           "offset": -5,
           "properties": {
             "labels": {
@@ -109,15 +108,39 @@ Sitbit.Views.CalsTile = Backbone.View.extend ({
           "properties": {
             "enter": {
               "x": {"scale": "x", "field": "x"},
-              "width": {"scale": "x", "band": true, "offset": -1},
-              "y": {"scale": "y", "field": "y"},
+              "width": {"scale": "x", "band": true},
+              "y": {"value": 0},
+              "y2": {"value": $("#cals-tile").height() - 75},
+              "fill": {"data": "table", "field": "zc"},
+              "fillOpacity": {"value": 0.1}
+            }
+          }
+        },
+        {
+          "type": "area",
+          "from": {"data": "integral"},
+          "properties": {
+            "enter": {
+              "interpolate": {"value": "basis"},
+              "x": {"scale": "x", "field": "x"},
+              "y": {"scale": "y", "field": "t"},
               "y2": {"scale": "y", "value": 0}
             },
             "update": {
-              "fill": {"data": "table", "field": "c"}
+              "fill": {
+                "r": {"value": 220},
+                "g": {"value": 220},
+                "b": {"value": 220}
+              },
+              "fillOpacity": {"value": 0.65}
             },
             "hover": {
-              "fill": {"data": "table", "field": "hc"}
+              "fill": {
+                "r": {"value": 190},
+                "g": {"value": 190},
+                "b": {"value": 190}
+              },
+              "fillOpacity": {"value": 0.9}
             }
           }
         },
@@ -127,15 +150,37 @@ Sitbit.Views.CalsTile = Backbone.View.extend ({
           "properties": {
             "enter": {
               "x": {"scale": "x", "value": hourNow},
-              "width": {"value": 2},
+              "width": {"scale": "x", "band": true, "offset": -1},
               "y": {"value": 0},
               "y2": {"value": $("#cals-tile").height() - 75}
             },
             "update": {
-              "fill": {"value": "red"}
+              "fillOpacity": {"value": 0.01},
+              "fill": {"value": "red"},
             },
             "hover": {
+              "fillOpacity": {"value": 0.1},
               "fill": {"value": "maroon"}
+            }
+          }
+        },
+        {
+          "type": "rect",
+          "from": {"data": "table"},
+          "properties": {
+            "enter": {
+              "x": {"scale": "x", "field": "x"},
+              "width": {"scale": "x", "band": true, "offset": -1},
+              "y": {"scale": "y", "field": "y"},
+              "y2": {"scale": "y", "value": 0}
+            },
+            "update": {
+              "fill": {"data": "table", "field": "c"},
+              "fillOpacity": {"value": 0.75}
+            },
+            "hover": {
+              "fill": {"data": "table", "field": "hc"},
+              "fillOpacity": {"value": 1.0}
             }
           }
         }
@@ -144,39 +189,61 @@ Sitbit.Views.CalsTile = Backbone.View.extend ({
     return graphSpecs;
   },
 
-  parseCalsData: function () {
+  parseCalsDiffs: function () {
     var sitData = this.sitsToday;
     var cutoff = new Date(Date.now()).getHours();
-    var user_rmr = this.user.escape('rmr');
     var output = new Array(24);
+    var integral = new Array(cutoff);
+    var user_rmr = this.user.escape('rmr');
+    var minDomain = -200;
+    var maxDomain = 200;
 
     for (var i = 0; i < 24; i++) {
-      var lastBar = 0;
-      if (i > 0) { lastBar = output[i - 1].t; }
       var bar = -user_rmr;
-      if (i > cutoff) { bar = 0; lastBar = 0; }
+      if (i > cutoff) { bar = 0; }
       output[i] = {
         "x": i,
         "y": bar,
-        "t": lastBar + bar,
-        "c": "lightgray",
-        "hc": "lightblue"
+        "t": 0,
+        "c": "gray",
+        "hc": "lightblue",
+        "zc": "transparent"
       };
+
+      if (lastBar > maxDomain) { maxDomain = (Math.round(lastBar/100)*100 + 100); }
+      if (lastBar < minDomain) { minDomain = (Math.round(lastBar/100)*100 - 100); }
     }
 
     sitData.forEach(function (sitdatum) {
       sitdatum.forEach(function (hourly) {
         if (hourly.date.day === new Date(Date.now()).getDate()) {
-          var idx = parseInt(hourly.h_start);
+          var idx = parseInt(hourly.h_start - 0.1);
           var frac = hourly.h_end - hourly.h_start;
           output[idx].y += hourly.scr * frac;
 
-          hourly.is_sleep ? output[idx].c = "darkgray" : output[idx].c = "gray";
+          // Ugly conditionals for color assignment:
           if (output[idx].y > 0) { output[idx].hc = "lightgreen"; }
+          if (hourly.is_sleep) {
+            output[idx].c = "black";
+            output[idx].zc = "steelblue";
+          } else {
+            output[idx].c = "gray";
+            output[idx].zc = "green";
+          }
         }
       }.bind(output));
     }.bind(output));
 
-    return output;
+    var lastBar = 0;
+    for (var z = 0; z <= cutoff; z++) {
+      if (z > 0) { lastBar += output[z - 1].y; }
+      integral[z] = { "x": z, "t": lastBar };
+
+      if ( integral[z].t > maxDomain) { maxDomain = (Math.round(integral[z].t/100)*100 + 100); }
+      if ( integral[z].t < minDomain) { minDomain = (Math.round(integral[z].t/100)*100 - 100); }
+    }
+
+    this.graphDomain = [minDomain, maxDomain];
+    return [output, integral];
   }
 });
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: follows
+#
+#  id         :integer          not null, primary key
+#  follow_id  :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Follow < ActiveRecord::Base
 end
